38d842bfac9a1c709d678a271611aa6fb335c5a8
Navneet Nandan <navneet.nandan8@gmail.com>
2016-02-29 14:01:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/iiitvstudents/music/NoteFrequency.java
@@ -0,0 +1,16 @@
+package com.iiitvstudents.music;
+
+/**
+ * Created by Navneet on 14-02-2016.
+ */
+public class NoteFrequency {
+    double frequency;
+    String note;
+    NoteFrequency(double frequency, String note){
+        this.frequency=frequency;
+        this.note=note;
+    }
+    NoteFrequency(){
+        ;
+    }
+}
