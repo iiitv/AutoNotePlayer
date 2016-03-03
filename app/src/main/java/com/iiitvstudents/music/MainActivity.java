3f837fd141e79cafeae8137fa1846a7575b13884
Navneet Nandan <navneet.nandan8@gmail.com>
2016-03-04 00:42:26 +0530

    Add MIDI Sequencer

--- app/src/main/java/com/iiitvstudents/music/MainActivity.java
+++ app/src/main/java/com/iiitvstudents/music/MainActivity.java
@@ -3,7 +3,16 @@ package com.iiitvstudents.music;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -15,16 +24,131 @@ import be.tarsos.dsp.io.android.AudioDispatcherFactory;
 import be.tarsos.dsp.pitch.PitchDetectionHandler;
 import be.tarsos.dsp.pitch.PitchDetectionResult;
 import be.tarsos.dsp.pitch.PitchProcessor;
+import jp.kshoji.javax.sound.midi.InvalidMidiDataException;
+import jp.kshoji.javax.sound.midi.MidiEvent;
+import jp.kshoji.javax.sound.midi.MidiSystem;
+import jp.kshoji.javax.sound.midi.MidiUnavailableException;
+import jp.kshoji.javax.sound.midi.Sequence;
+import jp.kshoji.javax.sound.midi.Sequencer;
+import jp.kshoji.javax.sound.midi.ShortMessage;
+import jp.kshoji.javax.sound.midi.Track;
 
 public class MainActivity extends AppCompatActivity {
     public static final int no_samples=4;
     int i=0,mid;
     float[] data=new float[no_samples];
+    final int SAMPLE_RATE=44100;
+    final int BUFFER_SIZE=7056;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            for (int i = 0; i <1000 ; i++) {
+                Sequencer sequencer= MidiSystem.getSequencer();
+                sequencer.open();
+                Sequence sequence=new Sequence(Sequence.PPQ,4);
+                Track track=sequence.createTrack();
+                ShortMessage a=new ShortMessage(144,1,44,100);
+                MidiEvent midion=new MidiEvent(a,1);
+                ShortMessage b=new ShortMessage(128,1,44,100);
+                MidiEvent midioff=new MidiEvent(b,20);
+                track.add(midion);
+                track.add(midioff);
+                sequencer.setSequence(sequence);
+                sequencer.start();
+            }
+
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
         final ArrayList<NoteFrequency> notes=new ArrayList<>();
+        addAllNotesFrequency(notes);
+        AudioDispatcher dispatcher = AudioDispatcherFactorywithAEC.fromDefaultMicrophone(SAMPLE_RATE, BUFFER_SIZE, 0);
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            float sum=0;
+            float mean_data;
+            @Override
+            public void handlePitch(PitchDetectionResult result,AudioEvent e) {
+                final float pitchInHz = result.getPitch();
+                Log.e("pitch",pitchInHz+"");
+                data[i++%no_samples]=pitchInHz;
+                sum+=pitchInHz;
+                if(i>=no_samples){
+//                    for (int j = 0; j <no_samples ; j++) {
+//                        sum=sum+data[j];
+//                    }
+//                    i=0;
+                     mean_data=sum/no_samples;
+                    sum-=data[(i)%no_samples];
+                    Log.e("Mean_data",""+mean_data);
+                    int start=0;
+                    int end=notes.size()-1;
+                    mid=(start+end)/2;
+                    while (mid!=end&&mid!=start){
+                        if(notes.get(mid).frequency<=mean_data){
+                   //         Log.e("fas",start+" "+end+" "+mid);
+                            start=mid;
+                            mid=(start+end)/2;
+                        }else{
+                     //       Log.e("fasfas",start+" "+end+" "+mid);
+                            end=mid;
+                            mid=(start+end)/2;
+                        }
+                    }
+                    if(Math.abs(mean_data-notes.get(start).frequency)<Math.abs(mean_data-notes.get(end).frequency)){
+                        mid=start;
+                    }
+                    else
+                        mid=end;
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView disp=(TextView)findViewById(R.id.textView);
+                        disp.setText(""+notes.get(mid).note);
+                        TextView text = (TextView) findViewById(R.id.textView11);
+                        text.setText("" +mean_data+"Hz");
+                        /*osc.setFreq((int) mean_data+1);
+                        textHz.setText(""+mean_data);
+                        seekFreq.setProgress(osc.getFreq() - Oscillator.MIN_FREQUENCY);*/
+
+                    }
+                });
+            }
+        };
+        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, SAMPLE_RATE, BUFFER_SIZE, pdh);
+        dispatcher.addAudioProcessor(p);
+        new Thread(dispatcher,"Audio Dispatcher").start();
+        /*osc = new Oscillator();
+
+        // View assignments and initial setup
+        textHz = (EditText) findViewById(R.id.text_hz);
+        textVol = (EditText) findViewById(R.id.text_vol);
+
+        buttonToggle = (ToggleButton) findViewById(R.id.toggle_play);
+
+        seekFreq = (SeekBar) findViewById(R.id.slider_freq);
+        // seekFreq starts at Oscillator.MIN_FREQUENCY, so subtract that from
+        // its max value
+        seekFreq.setMax(Oscillator.MAX_FREQUENCY - Oscillator.MIN_FREQUENCY);
+        // Sets the initial seekFreq progress
+        seekFreq.setProgress(osc.getFreq() - Oscillator.MIN_FREQUENCY);
+
+        seekVol = (SeekBar) findViewById(R.id.slider_vol);
+        seekVol.setMax(100);
+        seekVol.setProgress(50);
+
+        spinWave = (Spinner) findViewById(R.id.spinner_wave);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.wave_choices,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinWave.setAdapter(adapter);*/
+    }
+
+    private void addAllNotesFrequency(ArrayList<NoteFrequency> notes) {
         notes.add(new NoteFrequency(16.35,"C 0"));
         notes.add(new NoteFrequency(17.32,"C# 0"));
         notes.add(new NoteFrequency(18.35,"D 0"));
@@ -132,57 +256,130 @@ public class MainActivity extends AppCompatActivity {
         notes.add(new NoteFrequency(6644.88,"G# 8"));
         notes.add(new NoteFrequency(7040.00,"A 8"));
         notes.add(new NoteFrequency(7458.62,"A# 8"));
-        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
-        PitchDetectionHandler pdh = new PitchDetectionHandler() {
-            float sum=0;
-            float mean_data;
-            @Override
-            public void handlePitch(PitchDetectionResult result,AudioEvent e) {
-                final float pitchInHz = result.getPitch();
-                Log.e("pitch",pitchInHz+"");
-                data[i++%no_samples]=pitchInHz;
-                sum+=pitchInHz;
-                if(i>=no_samples){
-//                    for (int j = 0; j <no_samples ; j++) {
-//                        sum=sum+data[j];
-//                    }
-//                    i=0;
-                     mean_data=sum/no_samples;
-                    sum-=data[(i)%no_samples];
-                    Log.e("Mean_data",""+mean_data);
-                    int start=0;
-                    int end=notes.size()-1;
-                    mid=(start+end)/2;
-                    while (mid!=end&&mid!=start){
-                        if(notes.get(mid).frequency<=mean_data){
-                            Log.e("fas",start+" "+end+" "+mid);
-                            start=mid;
-                            mid=(start+end)/2;
-                        }else{
-                            Log.e("fasfas",start+" "+end+" "+mid);
-                            end=mid;
-                            mid=(start+end)/2;
-                        }
-                    }
-                    if(Math.abs(mean_data-notes.get(start).frequency)<Math.abs(mean_data-notes.get(end).frequency)){
-                        mid=start;
-                    }
-                    else
-                        mid=end;
-                }
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView disp=(TextView)findViewById(R.id.textView);
-                        disp.setText(""+notes.get(mid).note);
-                        TextView text = (TextView) findViewById(R.id.textView1);
-                        text.setText("" +mean_data+"Hz");
-                    }
-                });
-            }
-        };
-        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
-        dispatcher.addAudioProcessor(p);
-        new Thread(dispatcher,"Audio Dispatcher").start();
     }
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Updates the frequency once the textHz EditText loses focus
+        textHz.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    int freqValue = Integer.parseInt(((EditText) v).getText().toString());
+                    if (freqValue > Oscillator.MAX_FREQUENCY) {
+                        osc.setFreq(Oscillator.MAX_FREQUENCY);
+                    }
+                    else {
+                        osc.setFreq(freqValue);
+                    }
+
+                    // Since the SeekBar starts at Oscillator.MIN_FREQUENCY, it
+                    // needs to be subtracted
+                    // from the value (i.e. 20Hz gets set at 0 on seekFreq)
+                    seekFreq.setProgress(osc.getFreq() - Oscillator.MIN_FREQUENCY);
+                }
+            }
+        });
+
+        // Updates the volume from textVol once 'enter' has been pressed on the
+        // soft keyboard.
+        textVol.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    // Prevents a value greater than 100 from being sent to the
+                    // volume
+                    String value = ((EditText) v).getText().toString();
+                    int volValue = Integer.parseInt(value);
+                    if (volValue > 100) {
+                        osc.setVolume(100);
+                    }
+                    else {
+                        osc.setVolume(volValue);
+                    }
+                    seekVol.setProgress(osc.getVolume());
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        });
+
+        // Handles the pausing and playing of the oscillator from the
+        // ToggleButton
+        buttonToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (buttonView.isChecked()) {
+                    osc.play();
+                    new Thread(new Runnable() {
+                        public void run() {
+                            // Continuously fills the buffer while the
+                            // oscillator is playing
+                            while (osc.getIsPlaying()) {
+                                osc.fillBuffer();
+                            }
+                        }
+                    }).start();
+                }
+                else {
+                    osc.pause();
+                }
+            }
+        });
+
+        // Updates the oscillator's frequency based on the progress of seekFreq
+        seekFreq.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                osc.setFreq(Oscillator.MIN_FREQUENCY + seekBar.getProgress());
+                textHz.setText("" + osc.getFreq());
+            }
+        });
+
+        // Updates the oscillator's volume based on the progress of seekVol
+        seekVol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+            }
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                osc.setVolume(seekBar.getProgress());
+                textVol.setText("" + osc.getVolume());
+            }
+        });
+
+        // Updates the oscillator's waveform based on the selection in spinWave
+        spinWave.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                osc.setWave(pos);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    // Stops the oscillator playback and releases the AudioTrack resources
+    @Override
+    protected void onPause() {
+        super.onPause();
+        osc.stop();
+        buttonToggle.setChecked(false);
+    }
+    private EditText textHz, textVol;
+    private Oscillator osc;
+    private ToggleButton buttonToggle;
+    private SeekBar seekFreq, seekVol;
+    private Spinner spinWave;*/
 }
